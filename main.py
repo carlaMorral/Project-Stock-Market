ee6f1085bef51509c03e5ad593f6c0838ecc098d
Carla Morral Martin <31885862+carlaMorral@users.noreply.github.com>
2023-05-02 13:39:18 -0400

    created dates

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import json
+import jsonpickle
 import random
 import datetime
 import numpy as np
@@ -10,30 +11,39 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 
-companies = {}
-min_date = datetime.datetime.max
-max_date = datetime.datetime.min
-for file in os.listdir("data"):
-    comp = "data/" + file
-    if os.stat(comp).st_size == 0: continue
-    df = pd.read_csv(comp)
-    df = df.drop(columns=["Open", "Close", "Volume", "OpenInt"])
-    df["Price"] = (df["High"] + df["Low"]) / 2
-    df = df.drop(columns=["High", "Low"])
-    start_date = pd.to_datetime(df.loc[0,"Date"])
-    end_date = pd.to_datetime(df.iloc[-1,df.columns.get_loc("Date")])
-    if start_date < min_date: min_date = start_date
-    if end_date > max_date: max_date = end_date
-    companies[file[:-7]] = df
+# companies = {}
+# min_date = datetime.datetime.max
+# max_date = datetime.datetime.min
+# for file in os.listdir("data"):
+#     comp = "data/" + file
+#     if os.stat(comp).st_size == 0: continue
+#     df = pd.read_csv(comp)
+#     df = df.drop(columns=["Open", "Close", "Volume", "OpenInt"])
+#     df["Price"] = (df["High"] + df["Low"]) / 2
+#     df = df.drop(columns=["High", "Low"])
+#     start_date = pd.to_datetime(df.loc[0,"Date"])
+#     end_date = pd.to_datetime(df.iloc[-1,df.columns.get_loc("Date")])
+#     if start_date < min_date: min_date = start_date
+#     if end_date > max_date: max_date = end_date
+#     companies[file[:-7]] = df
+
+
+# dates = defaultdict(set)
+# for company, df in companies.items():
+#     for date in df["Date"]:
+#         dates[date].add(company)
+
+# jsondates = jsonpickle.encode(dates)
+# with open("dates.txt","w") as f:
+#     f.write(json.dumps(jsondates))
+
+with open("dates.txt","r") as f:
+    jsondata = f.read()
+dates = jsonpickle.decode(json.loads(jsondata))
 
 min_date = datetime.datetime(1962,1,2)
 max_date = datetime.datetime(2017,1,2)
 date_range = pd.date_range(start=min_date, end=max_date, freq='D')
-
-dates = defaultdict(set)
-for company, df in companies.items():
-    for date in df["Date"]:
-        dates[date].add(company)
 
 n = []
 t = []
@@ -42,6 +52,7 @@ delta = (max_date-min_date).days
 
 npoints = 0
 while npoints < 1000:
+    print(npoints)
     random_days_1 = random.randint(0, delta)
     random_date_1 = min_date + timedelta(days=random_days_1)
     random_days_2 = random.randint(0, delta-random_days_1)

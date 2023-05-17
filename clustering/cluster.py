d7f27b9fa614885fa54c71546c25935c8af1a608
Carla Morral Martin <31885862+carlaMorral@users.noreply.github.com>
2023-05-17 14:25:44 +0200

    update clusterings

--- clustering/cluster.py
+++ clustering/cluster.py
@@ -3,10 +3,9 @@ import pandas as pd
 import numpy as np
 
 from collections import defaultdict
-from distance import *
+import distance
 from plot import *
 from single_linkage import *
-from spectral import *
 
 
 def standarize_data(data):
@@ -14,8 +13,8 @@ def standarize_data(data):
     Computes Amplitude Scaling of the given time series
     """
     # compute the row-wise mean and std
-    mean = np.mean(data, axis=1, keepdims=True)
-    std = np.std(data, axis=1, keepdims=True)
+    mean = np.mean(data, axis=0, keepdims=True)
+    std = np.std(data, axis=0, keepdims=True)
 
     # subtract the mean and divide by the std
     data_norm = (data - mean) / std
@@ -33,29 +32,33 @@ def read_data():
     return data, grouped_df
 
 data, series = read_data()
-print(data.shape)
 
 # Single-linkage clustering
-eps = 500
-metric = euclidean
-data_norm = standarize_data(data)
-labels = single_linkage_clustering(data, metric, eps)
-print("Labels:")
-print(labels)
-clusters = defaultdict(list)
-for i, label in enumerate(labels):
-    value = index_name = series.index[i]
-    clusters[label].append(value)
-with open("sl_euclid.txt", "w") as sl:
-    for label, comps in clusters.items():
-        sl.write("Cluster: " + str(label) + ", companies: " + str(comps) + "\n")
+def single_linkage(metric, eps=20):
+    data_norm = standarize_data(data)
+    labels = single_linkage_clustering(data_norm, metric, eps)
+    return labels
+
+
+if __name__=="__main__":
+    method = sys.argv[1]
+    metric = sys.argv[2]
+
+    labels = locals()[method](getattr(distance, metric))
+    outfile = method + "_" + metric + ".txt"
+
+    clusters = defaultdict(list)
+    for i, label in enumerate(labels):
+        value = index_name = series.index[i]
+        clusters[label].append(value)
+    
+    with open(outfile, "w") as sl:
+        for label, comps in clusters.items():
+            sl.write("Cluster: " + str(label) + ", companies: " + str(comps) + "\n")
 
 # TODO probar mes metriques (Dynamic Time Warping?) pero es n**2, hauriem de reduir les dades
 
 
 # TODO spectral clustering
-# eps = 0.2
-# ggraph = get_input_graph(data, eps=eps, type="gaussian")
-# predg = spectral_clustering(k=2, graph=ggraph)
-# show(data, predg, "Spectral clustering - Gaussian graph prediction, eps=" + str(eps))
+
 
--- clustering/distance.py
+++ clustering/distance.py
@@ -1,6 +1,11 @@
 import numpy as np
+from dtaidistance import dtw
 from numpy import linalg as LA
 
 
 def euclidean(xi, xj):
     return LA.norm(xj-xi)
+
+
+def dynamic_time_warping(xi, xj):
+    return dtw.distance_fast(xi, xj, use_pruning=True)

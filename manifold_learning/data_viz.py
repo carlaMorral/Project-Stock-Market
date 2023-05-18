1512193852dce97f90f40529ed8bf864b6aef2ab
OneStone2 <jan@ventoso.org>
2023-05-17 20:00:43 -0700

    4d plot, for real

--- manifold_learning/data_viz.py
+++ manifold_learning/data_viz.py
@@ -35,6 +35,8 @@ def read_data():
 data, series = read_data()
 
 if __name__=="__main__":
+    # 1D embedding
+    """
     metric = distance.euclidean
     embedding = mds(metric=metric, d=1)
     data_norm = standarize_data(data)
@@ -48,5 +50,24 @@ if __name__=="__main__":
     ax.set_xlim3d(left=-50, right=50)
     ax.set_zlim3d(bottom=-2, top=2)
     plt.show()
+    """
+    
+    metric = distance.euclidean
+    embedding = mds(metric=metric, d=2)
+    data_norm = standarize_data(data)
+    x_dim, y_dim = data_norm.shape
+    t_col = np.tile(np.arange(y_dim), (x_dim, 1))
+    ts_data = np.stack((t_col.flatten(), data_norm.flatten()), axis=1)
+    manifold = np.concatenate((np.repeat(embedding, y_dim, axis=0), ts_data), axis=1)
+    for i in range(0, 3000, 300):
+        manifold_part = manifold[manifold[:,2] == i]
+        print(manifold_part.shape)
+        fig = plt.figure()
+        ax = plt.axes(projection='3d')
+        ax.scatter(manifold_part[:,0], manifold_part[:,1], manifold_part[:,3])
+        ax.set_xlim3d(left=-50, right=50)
+        ax.set_ylim3d(bottom=-50, top=150)
+        ax.set_zlim3d(bottom=-2, top=2)
+        plt.show()
 
  

dca2a2965c40a256a8471a26af07ba4ae6b16238
OneStone2 <jan@ventoso.org>
2023-05-17 20:00:18 -0700

    4d plot

--- clustering/distance.py
+++ clustering/distance.py
@@ -1,11 +1,17 @@
 import numpy as np
 from dtaidistance import dtw
 from numpy import linalg as LA
+from scipy.spatial.distance import correlation
 
 
 def euclidean(xi, xj):
     return LA.norm(xj-xi)
 
+def correlation_distance(xi, xj):
+    return correlation(xi, xj)
 
 def dynamic_time_warping(xi, xj):
     return dtw.distance_fast(xi, xj, use_pruning=True)
+
+def pairwise_distance(x, y, ord=2):
+    return np.linalg.norm(np.expand_dims(x, 1) - np.expand_dims(y, 0), axis=2)
